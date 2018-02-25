d7fea549b58037e8b58e3f1265b53f5cf2801247
Jose Aureliano <joseaurelianojr@gmail.com>
2018-02-25 12:34:58 -0300

    adding vim config to gitignore and aadding initial dependencies

--- api-core/project.clj
+++ api-core/project.clj
@@ -1,10 +1,14 @@
 (defproject api-core "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Scheduled Delivery API"
+  :url ""
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]
+                 [korma "0.4.3"]
+                 [org.postgresql/postgresql "42.2.1"]
+                 [jumblerg/ring.middleware.cors "1.0.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler api-core.handler/app}
   :profiles
